916c3d40d14ac3f54f3e73a2606b5e323a0ccb0f
JNRPB <josh@jnrplasteringandbuilding.com>
2026-01-25 20:26:25 +0000

    Fixed Job name not updating in sidebar, called onUpdate to communicate to upper levels

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -4,7 +4,9 @@ import JobOverview from "./JobCardTabs/JobOverview";
 function JobCard({ job, onUpdate }) {
   const [activeTab, setActiveTab] = useState(0);
 
-  return <>{activeTab === 0 && <JobOverview job={job} />}</>;
+  return (
+    <>{activeTab === 0 && <JobOverview job={job} onUpdate={onUpdate} />}</>
+  );
 }
 
 export default JobCard;
